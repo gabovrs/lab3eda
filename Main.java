920fb37fcc951119414085ae9ecc8ccd6e69ab5e
Gabriel Varas <gabrielvaras.ibaceta@gmail.com>
2025-05-14 14:15:35 -0400

    feat: GenerateData class

--- Main.java
+++ Main.java
@@ -1,7 +1,40 @@
 import java.util.ArrayList;
 
 public class Main {
-    public class Game {
+
+    public static class GenerateData {
+        String[] names = {"Dragon", "Empire", "Quest", "Galaxy", "Legends", "Warrior"};
+        String[] categories = {"Action", "Adventure", "RPG", "Strategy", "Simulation"};
+
+        private String generateRandomName() {
+            StringBuilder name = new StringBuilder();
+            for (int i = 0; i < 2; i++) {
+                name.append(names[(int) (Math.random() * names.length)]);
+            }
+            return name.toString();
+        }
+
+        public ArrayList<Game> generateData(int numGames) {
+            ArrayList<Game> games = new ArrayList<>();
+            for (int i = 0; i < numGames; i++) {
+                String name = generateRandomName();
+                String category = categories[(int) (Math.random() * categories.length)];
+                int price = ((int) (Math.random() * 69) + 1) * 1000;
+                int quality = (int) (Math.random() * 100);
+                games.add(new Game(name, category, price, quality));
+            }
+            return games;
+        }
+
+        public void printGames(ArrayList<Game> games) {
+            for (Game game : games) {
+                System.out.println("Name: " + game.getName() + ", Category: " + game.getCategory() +
+                        ", Price: " + game.getPrice() + ", Quality: " + game.getQuality());
+            }
+        }
+    }
+
+    public static class Game {
         private String name;
         private String category;
         private int price;
@@ -31,7 +64,7 @@ public class Main {
         }
     }
 
-    public class DateSet {
+    public static class DateSet {
         private ArrayList<Game> data;
         private String sortedByAttribute;
 
@@ -84,10 +117,46 @@ public class Main {
             this.sortedByAttribute = attribute;
             switch (algorithm) {
                 case "bubbleSort":
+                    for (int i = 0; i < data.size() - 1; i++) {
+                        for (int j = 0; j < data.size() - i - 1; j++) {
+                            if (attribute == "price" && data.get(j).getPrice() > data.get(j + 1).getPrice()) {
+                                Game temp = data.get(j);
+                                data.set(j, data.get(j + 1));
+                                data.set(j + 1, temp);
+                            } else if (attribute == "quality" && data.get(j).getQuality() > data.get(j + 1).getQuality()) {
+                                Game temp = data.get(j);
+                                data.set(j, data.get(j + 1));
+                                data.set(j + 1, temp);
+                            }
+                        }
+                    }
                     break;
                 case "insertionSort":
+                    for (int i = 1; i < data.size(); i++) {
+                        Game key = data.get(i);
+                        int j = i - 1;
+                        while (j >= 0 && ((attribute == "price" && data.get(j).getPrice() > key.getPrice()) ||
+                                (attribute == "quality" && data.get(j).getQuality() > key.getQuality()))) {
+                            data.set(j + 1, data.get(j));
+                            j--;
+                        }
+                        data.set(j + 1, key);
+                    }
                     break;
                 case "selectionSort":
+                    for (int i = 0; i < data.size() - 1; i++) {
+                        int minIndex = i;
+                        for (int j = i + 1; j < data.size(); j++) {
+                            if (attribute == "price" && data.get(j).getPrice() < data.get(minIndex).getPrice()) {
+                                minIndex = j;
+                            } else if (attribute == "quality" && data.get(j).getQuality() < data.get(minIndex).getQuality()) {
+                                minIndex = j;
+                            }
+                        }
+                        Game temp = data.get(minIndex);
+                        data.set(minIndex, data.get(i));
+                        data.set(i, temp);
+                    }
                     break;
                 case "mergeSort":
                     break;
@@ -101,6 +170,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
+        GenerateData generateData = new GenerateData();
+        ArrayList<Game> games = generateData.generateData(100);
+        generateData.printGames(games);
     }
 }
